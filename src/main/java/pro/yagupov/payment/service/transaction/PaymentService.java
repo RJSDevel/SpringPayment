f8fdbb0ffd6d2fb3b1bd5b692c1324503bfb6023
Yagupov Ruslan <yagupov.ruslan@gmail.com>
2017-09-26 16:09:38 +0000

    Refactoring

--- src/main/java/pro/yagupov/payment/service/transaction/PaymentService.java
+++ src/main/java/pro/yagupov/payment/service/transaction/PaymentService.java
@@ -76,15 +76,15 @@ public class PaymentService {
         return transaction;
     }
 
-    @Retryable(backoff = @Backoff(delay = 2000))
     @Transactional
+    @Retryable(backoff = @Backoff(delay = 2000))
     public TransactionTDO authorizeTransaction(User pUser, TransactionTDO pTransaction) {
         pTransaction.setOperation(Transaction.Operation.AUTHORIZE);
         return new TransactionTDO(transactionProcessorManager.processing(preProcessingChecks(pUser, pTransaction)));
     }
 
-    @Retryable(backoff = @Backoff(delay = 2000))
     @Transactional
+    @Retryable(backoff = @Backoff(delay = 2000))
     public Transaction captureTransaction(User pUser, TransactionTDO pTransaction) {
 
         pTransaction.setOperation(Transaction.Operation.CAPTURE);
@@ -100,20 +100,20 @@ public class PaymentService {
         return transactionProcessorManager.processing(preProcessingChecks(pUser, pTransaction));
     }
 
-    @Retryable(backoff = @Backoff(delay = 2000))
     @Transactional
+    @Retryable(backoff = @Backoff(delay = 2000))
     public Transaction refundTransaction(User pUser, TransactionTDO pTransaction) {
         return null;
     }
 
-    @Retryable(backoff = @Backoff(delay = 2000))
     @Transactional
+    @Retryable(backoff = @Backoff(delay = 2000))
     public Transaction voidTransaction(User pUser, TransactionTDO pTransaction) {
         return null;
     }
 
-    @Retryable(backoff = @Backoff(delay = 2000))
     @Transactional
+    @Retryable(backoff = @Backoff(delay = 2000))
     public List<Transaction> getAllTransactionsByUser(User pUser) {
 
         List<Transaction> transactions = new ArrayList<>();
